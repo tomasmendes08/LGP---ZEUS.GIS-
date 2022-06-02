536abf5dbfa5e52d5bf6b59e7d8e78ff1ce2ce9f
Tomás <tomaslouromendes@gmail.com>
2022-06-02 02:33:13 +0100

    contact page

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -6,18 +6,32 @@ function ContactPage() {
   return (
     <>
     <NavbarComponent/>
-    <div className="contactPage container">
-        <div class="contactPageContainer mt-5 rounded">
+    <div className="contactPage container w-75">
+        <div class="contactPageContainer mt-5">
             <div class="contactPageHeader d-flex justify-content-center">
                 <h1 class="fw-bold text-white mt-3">Contact Us</h1>
             </div>
             <div class="contactPageInfo mt-3">
                 <form class="contactPageForm container">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label text-white">Email</label>
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email"/>
+                    <div class="mb-3 container">
+                        <label for="inputName" class="form-label text-white">Name*</label>
+                        <input type="text" class="form-control" id="inputName" placeholder="Enter your name" required/>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-3">Submit</button>
+                    <div class="mb-3 container">
+                        <label for="inputEmail" class="form-label text-white">Email*</label>
+                        <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email" required/>
+                    </div>
+                    <div class="mb-3 container">
+                        <label for="inputPhone" class="form-label text-white">Phone</label>
+                        <input type="tel" class="form-control" id="inputPhone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Enter your phone number"></input>
+                    </div>
+                    <div class="mb-4 container">
+                        <label for="inputTextarea" class="form-label text-white">Message*</label>
+                        <textarea class="form-control" id="inputTextarea" rows="5" required></textarea>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-light mb-3">Submit</button>
+                    </div>
                 </form>
             </div>
         </div>
